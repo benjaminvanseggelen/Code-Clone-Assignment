3cc94b9d81ec18cf81800b4d5404941650a1b777
Benjamin van Seggelen <benjaminvanseggelen@hotmail.com>
2022-03-08 12:58:57 +0100

    Loc counting

--- scripts/count-loc.py
+++ scripts/count-loc.py
@@ -3,15 +3,38 @@ import csv
 import os
 
 releases = []
+releases_loc = []
 
 if __name__ == "__main__":
     with open('../jquery-data/jquery_releases.csv', mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             releases.append(row)
-    
+
+        for release in releases:
+            print(f"Executing Cloc for release {release['tag']}")
+            src_loc_csv = csv.reader(os.popen(f"cloc --csv --exclude-lang=CSS,HTML --quiet ../jquery-data/{release['tag']}/src").read().split('\n'))
+            external_loc_csv = csv.reader(os.popen(f"cloc --csv --exclude-lang=CSS,HTML --quiet ../jquery-data/{release['tag']}/external").read().split('\n'))
+
+            src_loc = 0
+            external_loc = 0
+
+            for row in src_loc_csv:
+                if len(row) >= 2 and row[1] == 'Javascript':
+                    src_loc = int(row[4])
+                    break
+
+            for row in external_loc_csv:
+                if len(row) >= 2 and row[1] == 'Javascript':
+                    external_loc = int(row[4])
+                    break
+
+            total_loc = src_loc + external_loc
+            releases_loc.append({
+                'tag': release['tag'],
+                'loc': total_loc
+            })
 
 
-    for release in releases:
-        print(f"Executing Cloc for release {release['tag']}")
-        os.system(f"cloc ../jquery-data/{release['tag']}/")
+    with open('loc.json', 'w') as json_f:
+        json.dump(releases_loc, json_f)
