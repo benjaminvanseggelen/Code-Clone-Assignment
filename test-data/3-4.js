52f3ab0cc7347b66b1a9d367ca0a96311a31b56c
Tom Groot <4732713+Tomgroot@users.noreply.github.com>
2022-03-08 15:32:47 +0100

    4th less subtle

--- test-data/3-4.js
+++ test-data/3-4.js
@@ -1,7 +1,2 @@
-function div(a, b) {
-    if (b == 0) {  let v = 1; return Infinity; } else if (b < 0) { return - div(a, -b); }
-    let i = 0;
-    let x = b;
-    while (x <= a) {  v += 1;  x += b; i ++; }
-    return i;
-}
+//we calculate the division by removing all characters that we don't need and change some statements from negations to -1
+function d(y,z){if(z == 0){return Infinity;}else if(z < 0){return-1*d(y,-1*z);}let i=0;let x=z;while(x <= y){x+=z;i+=1;}return i;} 
